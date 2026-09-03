a20bfda73b373ce4cb68f575636feb9ebac0a099
shahprince2006p <shahlaxmi1986@gmail.com>
2026-09-03 11:20:19 +0530

    test submited

---
+++ script.js
@@ -0,0 +1,8 @@
+const arr=[];
+const form=document.getElementById("#studentForm")
+const student=document.getElementById("#studentname")
+
+const email=document.getElementById("#email")
+const number=document.getElementById("#phonenumber")
+const dob=document.getElementById("#dob")
+const =document.getElementById()
