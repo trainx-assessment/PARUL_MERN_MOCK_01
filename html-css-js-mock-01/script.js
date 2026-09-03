444994a1847ed137cd880601e86ebdf9c97847c4
Prashant Chauhan <prashantchauhanp2@gmail.com>
2026-09-03 10:42:59 +0530

    committed

---
+++ html-css-js-mock-01/script.js
@@ -0,0 +1,33 @@
+let data=[{
+    "id": 1,
+    "name": "...",
+    "email": "...",
+    "phone": "...",
+    "dob": "...",
+    "gender": "...",
+    "course": "...",
+    "skills": ["HTML", "CSS"],
+    "about": "...",
+    "photo": "..."
+}]
+let register = document.querySelector(".register");
+register.addEventListener("onclick",()=>{
+//     let demo = {
+//     "id": 1,
+//     "name": "...",
+//     "email": "...",
+//     "phone": "...",
+//     "dob": "...",
+//     "gender": "...",
+//     "course": "...",
+//     "skills": ["HTML", "CSS"],
+//     "about": "...",
+//     "photo": "..."
+// }
+// let name = document.querySelector(".Student Name")
+// demo.name = name.innerTEXT;
+let email = document.querySelector(".Email");
+// demo.email = email.innerHTML;
+console.log(email.innerHTML);
+})
+
