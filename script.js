d5ab059521f4350d24d4459c257aa3450110467e
Venu kumar <nalaparajuvenu@gmail.com>
2026-09-03 11:29:01 +0530

    my task

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form");
+const aboutStudent = document.querySelector("#aboutStudent");
+const characterCount = document.querySelector("#characterCount");
+
+
+const students = [];
+
+let nextStudentId = 1;
+let editingStudentId = null;
+
