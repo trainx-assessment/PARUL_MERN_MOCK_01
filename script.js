f0321bfb2da03eb7048c0f470b8ae1cfb36d5b89
Santosh-Prasad-Verma <santoshkumar995826@gmail.com>
2026-09-03 11:29:02 +0530

    2403031250025

---
+++ script.js
@@ -0,0 +1,23 @@
+// Suggested Time: 30 Minutes
+// When the form is submitted:
+
+// Prevent default form submission.
+// Read all form values.
+// Validate every required field.
+// Display validation messages.
+// Stop submission when validation fails.
+// Remove old validation messages when the user fixes the input.
+// Do not use only browser default validation.
+
+// Implement validation using JavaScript.
+
+// Student Name Validation
+// Rules:
+
+// Required
+// Minimum 3 characters
+// Maximum 40 characters
+// Only letters and spaces allowed
+// Numbers are not allowed
+// Special characters are not allowed
+
