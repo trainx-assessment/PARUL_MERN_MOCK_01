c207f6e437e0992c1e92527d0ea0cf4d239c342e
ZaidAjmeri07 <zaidajmeri201@gmail.com>
2026-09-03 11:28:45 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const name = document.querySelector(".Name");
+const email = document.querySelector(".Email");
+const phonenumber = document.querySelector(".phoneNumber");
+const DOB = document.querySelector(".DOB");
+const gender = document.querySelector(".gender");
+const course = document.querySelector(".course");
+
+const skills = document.querySelector(".skills");
+const photo = document.querySelector(".photo");
+
+const submit = document.getElementById("#submit");
+const reset = document.getElementById("#reset");
+
+const Form  = document.querySelector("app1");
+
+const vname = document.getElementById("vname");
+const vemail = document.getElementById("vemail");
+const vphone = document.getElementById("vphone");
+const vdob = document.getElementById("vdob");
+
+const vmale = document.getElementById("male");
+const vfemale = document.getElementById("female");
+const vOther = document.getElementById("Other");
+
+const vcourse = document.getElementById("Course");
+
+const vsiklls = document.getElementById("vskills");
+
+const vabout = document.getElementById("vabout");
+
+const vprofil_photo = document.getElementById("vprofil-photo");
+
+
+Form.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+    
+
+
+})
+
+
+const student = [];
